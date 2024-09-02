3cc06ffe854683dac9b3330f593409676a403dff
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-09-02 23:01:25 +0530

    update

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,12 +1,18 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads'));
+        cb(null, uploadDir); // Directory where files will be saved
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, `${Date.now()}-${file.originalname}`); // Rename the file to avoid conflicts
     }
 });
 
